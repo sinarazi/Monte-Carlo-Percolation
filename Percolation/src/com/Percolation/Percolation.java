92c7349b43e991d99fdd720a0ab26a70b5d036ac
sinarazi <razisina78@gmail.com>
2020-04-23 12:47:14 +0430

    Java files added

---
+++ Percolation/src/com/Percolation/Percolation.java
@@ -0,0 +1,165 @@
+package com.Percolation;
+
+// Importing WeightedQuickUnionUF library of algs4.jar file.
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class Percolation
+    {
+    final private WeightedQuickUnionUF grid;
+    final private WeightedQuickUnionUF full;
+    final private int n; // number
+    final private int top;
+    final private int bottom;
+    private boolean[] openNodes; // to check opening of nodes
+
+
+
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N)
+    {
+         if (N <= 0) // handle exception of invalid argument
+           throw new java.lang.IllegalArgumentException("Sorry, N should be greater than 0 :/ .");
+
+
+        grid = new WeightedQuickUnionUF(N * N + 2);  // call WeightedQuickUnionUF
+        full = new WeightedQuickUnionUF(N * N + 1);
+
+        this.n = N; // object
+
+        top = getSingleArrayIndex(N, N) + 1;
+        bottom = getSingleArrayIndex(N, N) + 2;
+        openNodes = new boolean[N * N];
+    }
+
+
+
+    private int getSingleArrayIndex(int i, int j)
+    {
+        doOutOfBoundsCheck(i, j);
+
+        return (n * (i - 1) + j) - 1;
+    }
+
+
+    // to implement IndexOutOfBoundsException
+    private void doOutOfBoundsCheck(int i, int j)
+    {
+        if (!isValid(i, j))
+            throw new IndexOutOfBoundsException("Sorry, Values not found :/ .");
+    }
+
+
+
+    // to check validation
+    private boolean isValid(int i, int j)
+    {
+        return i > 0 && j > 0 && i <= n && j <= n;
+    }
+
+
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j)
+    {
+        doOutOfBoundsCheck(i, j);
+
+        if (isOpen(i, j))
+        {
+            return; // No need to open this again as it's already open
+        }
+
+        int index = getSingleArrayIndex(i, j);
+        openNodes[index] = true;
+
+        if (i == 1)
+        {
+            grid.union(top, index); // using union method of WeightedQuickUnionUF
+            full.union(top, index);
+        }
+
+        if (i == n)
+        {
+            grid.union(bottom, index);
+        }
+
+
+        if (isValid(i - 1, j) && isOpen(i - 1, j))
+        {
+            grid.union(getSingleArrayIndex(i - 1, j), index);
+            full.union(getSingleArrayIndex(i - 1, j), index);
+        }
+
+        if (isValid(i, j + 1) && isOpen(i, j + 1))
+        {
+            grid.union(getSingleArrayIndex(i, j + 1), index);
+            full.union(getSingleArrayIndex(i, j + 1), index);
+        }
+
+        if (isValid(i + 1, j) && isOpen(i + 1, j))
+        {
+            grid.union(getSingleArrayIndex(i + 1, j), index);
+            full.union(getSingleArrayIndex(i + 1, j), index);
+
+        }
+
+        if (isValid(i, j - 1) && isOpen(i, j - 1))
+        {
+            /* Merges the set containing element
+            {@code p} with the set containing element {@code q}.
+             */
+            grid.union(getSingleArrayIndex(i, j - 1), index);
+            full.union(getSingleArrayIndex(i, j - 1), index);
+        }
+    }
+
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j)
+    {
+        doOutOfBoundsCheck(i, j);
+
+        return openNodes[getSingleArrayIndex(i, j)];
+    }
+
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j)
+    {
+        int index = getSingleArrayIndex(i, j);
+
+        return connect(index, top); // Returns true if the two elements are in the same set
+    }
+
+
+    /* does the system percolate? and check
+    the connection of top and bottom.
+    */
+    public boolean percolates()
+    {
+        return connect(top, bottom); // to fix deprecated methods in java api,
+                                     // I have to use connect method instead of using connected
+                                     // method of WeightedQuickUnionUF
+
+    }
+
+    public boolean connect(int p, int q)
+    {
+        return grid.find(p) == grid.find(q);
+    }
+/*
+    // to count number of open sites in order to calculate the mean
+    public int numberOfOpenSites()
+    {
+        int openSites = 0;
+
+        for(int row = 1; row <= n; row++)
+            for (int cul = 1; cul <= n; cul++)
+                if (isOpen(row, cul))
+                    openSites++;
+
+        return openSites;
+    }
+
+ */
+}
+
---
+++ Percolation/test/PercolationStats.java
@@ -0,0 +1,118 @@
+
+
+import edu.princeton.cs.algs4.StdRandom; // Importing StdRandom library of algs4.jar file.
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
+
+// perform T independent experiments on an N-by-N grid
+public class PercolationStats
+    {
+    private final int n;
+    private final int t;
+    private final double[] perStats;
+
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T)
+    {
+        if (N <= 0 && T <= 0)
+        {
+            throw new IllegalArgumentException("Sorry, N and T should be greater than 0");
+        }
+        else
+            {
+            this.n = N;
+            this.t = T;
+            this.perStats = new double[T];
+
+            for(int j = 0; j < T; ++j)
+            {
+                Percolation P = new Percolation(N);
+
+                int i;
+                for(i = 0; !P.percolates(); ++i)
+                {
+                    this.openRandomNode(P);
+                }
+
+                this.perStats[j] = (double)i / (double)(N * N); // To gain average of percolate system
+            }
+        }
+    }
+
+
+    // sample mean of percolation threshold
+    public double mean()
+    {
+        return StdStats.mean(this.perStats);
+    }
+
+
+    // sample standard deviation of percolation threshold
+    public double stddev()
+    {
+        return StdStats.stddev(this.perStats);
+    }
+
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo()
+    {
+        return this.mean() - 1.96 * this.stddev() / Math.sqrt((double)this.t);
+    }
+
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi()
+    {
+        return this.mean() + 1.96 * this.stddev() / Math.sqrt((double)this.t);
+    }
+
+
+    // Open random nodes
+    private void openRandomNode (Percolation N) {
+        boolean T = true;
+        int j = 0;
+
+        int i;
+        for(i = 0; T; T = N.isOpen(j, i))
+        {
+            /* Call uniform method of StdRandom inorder to get
+             a random long integer uniformly in [0, n)
+             */
+            j = StdRandom.uniform(1, this.n + 1);
+            i = StdRandom.uniform(1, this.n + 1);
+        }
+
+        N.open(j, i);
+    }
+
+
+    // test client, described below
+    public static void main(String[] args)
+    {
+        // Integer.parseInt(args[0])
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        
+        // to measure time of project
+        Stopwatch timeW = new Stopwatch();
+
+        PercolationStats ps = new PercolationStats(N, T);
+        
+        System.out.println("mean:\t\t\t\t\t= " + ps.mean());
+        System.out.println("stddev:\t\t\t\t\t= " + ps.stddev());
+
+        double var = ps.confidenceLo();
+        System.out.println("95% confidence interval = " + var + ", " + ps.confidenceHi());
+
+
+        /* Call elapsedTime method of Stopwatch inorder to elapse
+         CPU time (in seconds) since the stopwatch was created
+         */
+        double time = timeW.elapsedTime();
+        System.out.println("The total time is:\t" + time); // Print total time
+
+    }
+}
